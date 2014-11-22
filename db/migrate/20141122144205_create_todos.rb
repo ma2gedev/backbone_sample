498625ff040452a85318af4acf6313e9aee0fd7b
Takayuki Matsubara <takayuki.1229@gmail.com>
2014-11-22 23:43:53 +0900

    `rails g scaffold todo name:string --force`

--- db/migrate/20141122144205_create_todos.rb
+++ db/migrate/20141122144205_create_todos.rb
@@ -1,6 +1,7 @@
 class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
+      t.string :name
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122143110) do
+ActiveRecord::Schema.define(version: 20141122144205) do
 
   create_table "todos", force: true do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
